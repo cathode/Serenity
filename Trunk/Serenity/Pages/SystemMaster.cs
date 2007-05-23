d9edc87af0e9e4393bec4c382a4d1602a79f7d44
wshelley <wshelley@7c140d53-a5f7-2844-acf0-7413b3fec49d>
2007-05-23 04:28:58 +0000

    git-svn-id: svn://gearedstudios.com/serenity@30 7c140d53-a5f7-2844-acf0-7413b3fec49d

--- Trunk/Serenity/Pages/SystemMaster.cs
+++ Trunk/Serenity/Pages/SystemMaster.cs
@@ -2,10 +2,19 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
+using Serenity.Web;
+
 namespace Serenity.Pages
 {
     internal sealed class SystemMaster : MasterPage
     {
-
+        public override void PostRequest(CommonContext context)
+        {
+            base.PostRequest(context);
+        }
+        public override void PreRequest(CommonContext context)
+        {
+            base.PreRequest(context);
+        }
     }
 }
