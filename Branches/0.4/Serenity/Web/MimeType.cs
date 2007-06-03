db6e6aac403e26b352a5fd41fdb1b94516085028
wshelley <wshelley@7c140d53-a5f7-2844-acf0-7413b3fec49d>
2007-06-03 18:57:22 +0000

    Added some common mimetypes to Serenity.Web.MimeType as static properties.
    git-svn-id: svn://gearedstudios.com/serenity@46 7c140d53-a5f7-2844-acf0-7413b3fec49d

--- Branches/0.4/Serenity/Web/MimeType.cs
+++ Branches/0.4/Serenity/Web/MimeType.cs
@@ -4,11 +4,219 @@ using System.Text;
 
 namespace Serenity.Web
 {
-    public class MimeType
+    /// <summary>
+    /// Represents a MIME type, usually used to identify the type of content in a message.
+    /// </summary>
+    public struct MimeType
     {
-        public static string Format(string primary, string secondary)
+        #region Constructors - Public
+        public MimeType(string type, string subtype)
         {
-            return primary + "/" + secondary;
+            //WS: add input-validation here.
+            this.type = type;
+            this.subtype = subtype;
         }
+        #endregion
+        #region Fields - Private
+        private string subtype;
+        private string type;
+        #endregion
+
+        #region Properties - Public
+        /// <summary>
+        /// application/EDI-X12
+        /// </summary>
+        public static MimeType ApplicationEdiX12
+        {
+            get
+            {
+                return new MimeType("application", "EDI-X12");
+            }
+        }
+        /// <summary>
+        /// application/EDIFACT
+        /// </summary>
+        public static MimeType ApplicationEdiFact
+        {
+            get
+            {
+                return new MimeType("application", "EDIFACT");
+            }
+        }
+        /// <summary>
+        /// application/javascript
+        /// </summary>
+        public static MimeType ApplicationJavascript
+        {
+            get
+            {
+                return new MimeType("application", "javascript");
+            }
+        }
+        /// <summary>
+        /// application/octet-stream
+        /// </summary>
+        public static MimeType ApplicationOctetStream
+        {
+            get
+            {
+                return new MimeType("application", "octet-stream");
+            }
+        }
+        /// <summary>
+        /// application/ogg
+        /// </summary>
+        public static MimeType ApplicationOgg
+        {
+            get
+            {
+                return new MimeType("application", "ogg");
+            }
+        }
+        /// <summary>
+        /// application/xhtml+xml
+        /// </summary>
+        public static MimeType ApplicationXhtmlPlusXml
+        {
+            get
+            {
+                return new MimeType("application", "xhtml+xml");
+            }
+        }
+        /// <summary>
+        /// application/xml
+        /// </summary>
+        public static MimeType ApplicationXml
+        {
+            get
+            {
+                return new MimeType("application", "xml");
+            }
+        }
+        /// <summary>
+        /// application/x-shockwave-flash
+        /// </summary>
+        public static MimeType ApplicationXShockwaveFlash
+        {
+            get
+            {
+                return new MimeType("application", "x-shockwave-flash");
+            }
+        }
+        /// <summary>
+        /// audio/mpeg
+        /// </summary>
+        public static MimeType AudioMpeg
+        {
+            get
+            {
+                return new MimeType("audio", "mpeg");
+            }
+        }
+        /// <summary>
+        /// audio/x-ms-wma
+        /// </summary>
+        public static MimeType AudioXMSWma
+        {
+            get
+            {
+                return new MimeType("audio", "x-ms-wma");
+            }
+        }
+        /// <summary>
+        /// audio/vnd.rn-realaudio
+        /// </summary>
+        public static MimeType AudioVendorRNRealAudio
+        {
+            get
+            {
+                return new MimeType("audio", "vnd.rn-realaudio");
+            }
+        }
+        /// <summary>
+        /// audio/x-wav
+        /// </summary>
+        public static MimeType AudioXWav
+        {
+            get
+            {
+                return new MimeType("audio", "x-wav");
+            }
+        }
+        /// <summary>
+        /// image/gif
+        /// </summary>
+        public static MimeType ImageGif
+        {
+            get
+            {
+                return new MimeType("image", "gif");
+            }
+        }
+        /// <summary>
+        /// image/jpeg
+        /// </summary>
+        public static MimeType ImageJpeg
+        {
+            get
+            {
+                return new MimeType("image", "jpeg");
+            }
+        }
+        /// <summary>
+        /// image/png
+        /// </summary>
+        public static MimeType ImagePng
+        {
+            get
+            {
+                return new MimeType("image", "png");
+            }
+        }
+        /// <summary>
+        /// image/tiff
+        /// </summary>
+        public static MimeType ImageTiff
+        {
+            get
+            {
+                return new MimeType("image", "tiff");
+            }
+        }
+        /// <summary>
+        /// image/vnd.microsoft.icon
+        /// </summary>
+        public static MimeType ImageVendorMicrosoftIcon
+        {
+            get
+            {
+                return new MimeType("image", "vnd.microsoft.icon");
+            }
+        }
+        /// <summary>
+        /// multipart/mixed
+        /// </summary>
+        public static MimeType MultipartMixed
+        {
+            get
+            {
+                return new MimeType("multipart", "mixed");
+            }
+        }
+        public static MimeType MultipartAlternative
+        {
+            get
+            {
+                return new MimeType("multipart", "alternative");
+            }
+        }
+        public static MimeType MultipartRelated
+        {
+            get
+            {
+                return new MimeType("multipart", "related");
+            }
+        }
+        #endregion
     }
 }
