ee1fffc5ade9f455f0929e6df5841399efceed7b
WShelley <WShelley@7c140d53-a5f7-2844-acf0-7413b3fec49d>
2007-08-15 06:58:51 +0000

    * Added DomainSetting config file for root domain. * Removed Server/OperatingModes/DebugMode.cs because it was unused * Removed system/ResourceClasses/temp.html because it was temporary. * Updated copyright information in all code files to reflect new change to Microsoft Permissive License (from Microsoft Community License). * Changed license.txt from Ms-CL to Ms-PL.
    git-svn-id: svn://gearedstudios.com/serenity@99 7c140d53-a5f7-2844-acf0-7413b3fec49d

--- SerenityWeb/Branches/0.4/Serenity/Cache/CacheManager.cs
+++ SerenityWeb/Branches/0.4/Serenity/Cache/CacheManager.cs
@@ -1,15 +1,11 @@
-/*
-Serenity - The next evolution of web server technology
-Serenity/Cache/CacheManager.cs
-Copyright © 2006-2007 Serenity Project (http://SerenityProject.net/)
-
-This file is protected by the terms and conditions of the
-Microsoft Community License (Ms-CL), a copy of which should
-have been distributed along with this software. If not,
-you may find the license information at the following URL:
-
-http://www.microsoft.com/resources/sharedsource/licensingbasics/communitylicense.mspx
-*/
+/******************************************************************************
+ * Serenity - The next evolution of web server technology.                    *
+ * Copyright © 2006-2007 Serenity Project - http://SerenityProject.net/       *
+ *----------------------------------------------------------------------------*
+ * This software is released under the terms and conditions of the Microsoft  *
+ * Permissive License (Ms-PL), a copy of which should have been included with *
+ * this distribution as License.txt.                                          *
+ *****************************************************************************/
 using System;
 using System.Collections.Generic;
 using System.IO;
