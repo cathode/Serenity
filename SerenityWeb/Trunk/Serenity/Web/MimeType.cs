3533871f5e14908291f92b31bcb89dfc2316f18b
WShelley <WShelley@7c140d53-a5f7-2844-acf0-7413b3fec49d>
2007-08-28 07:11:10 +0000

    * Added new Services namespace to Serenity.Web, with new skeleton class WebService. * Updated most files in Serenity.Web with correct license header.
    git-svn-id: svn://gearedstudios.com/serenity@131 7c140d53-a5f7-2844-acf0-7413b3fec49d

--- SerenityWeb/Trunk/Serenity/Web/MimeType.cs
+++ SerenityWeb/Trunk/Serenity/Web/MimeType.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/******************************************************************************
+ * Serenity - The next evolution of web server technology.                    *
+ * Copyright © 2006-2007 Serenity Project - http://SerenityProject.net/       *
+ *----------------------------------------------------------------------------*
+ * This software is released under the terms and conditions of the Microsoft  *
+ * Permissive License (Ms-PL), a copy of which should have been included with *
+ * this distribution as License.txt.                                          *
+ *****************************************************************************/
+using System;
 using System.Collections.Generic;
 using System.Text;
 
