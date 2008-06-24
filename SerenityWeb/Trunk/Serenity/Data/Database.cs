74b12711f6d0978cc299bf7dbd7ab0688944c233
WShelley <WShelley@7c140d53-a5f7-2844-acf0-7413b3fec49d>
2008-06-24 06:50:13 +0000

    * Cleaned up Serenity.Data.Database class.
    git-svn-id: svn://gearedstudios.com/serenity@232 7c140d53-a5f7-2844-acf0-7413b3fec49d

--- SerenityWeb/Trunk/Serenity/Data/Database.cs
+++ SerenityWeb/Trunk/Serenity/Data/Database.cs
@@ -32,41 +32,23 @@ namespace Serenity.Data
             {
                 return true;
             }
-            else if (scope == DataScope.Global)
+            else
             {
-                SQLiteConnection.CreateFile(Database.GlobalDBPath);
-                if (File.Exists(Database.GlobalSchemaPath))
+                string datafilePath = Database.GetDatafilePath(scope);
+                string schemaPath = Database.GetSchemaPath(scope);
+                SQLiteConnection.CreateFile(datafilePath);
+                if (File.Exists(schemaPath))
                 {
-                    SQLiteConnection conn = new SQLiteConnection("Data Source=" + Database.GlobalDBPath);
-                    SQLiteCommand cmd = new SQLiteCommand(File.ReadAllText(Database.GlobalSchemaPath), conn);
+                    SQLiteConnection conn = new SQLiteConnection("Data Source=" + datafilePath);
+                    SQLiteCommand cmd = new SQLiteCommand(File.ReadAllText(schemaPath), conn);
                     conn.Open();
                     cmd.ExecuteNonQuery();
                     conn.Clone();
                     conn.Dispose();
                     cmd.Dispose();
+                    return true;
                 }
-                return true;
-            }
-            //TODO: Implement support for multiple scopes.
-            else if (scope == DataScope.Module)
-            {
-                if (Module.Current == null)
-                {
-                    return false;
-                }
-                throw new NotImplementedException();
-            }
-            else if (scope == DataScope.Domain)
-            {
-                if (Domain.Current == null || Module.Current == null)
-                {
-                    return false;
-                }
-                throw new NotImplementedException();
-            }
-            else
-            {
-                throw new ArgumentException(__Strings.Exceptions.UnrecognizedDataScope);
+                return false;
             }
         }
         /// <summary>
@@ -76,80 +58,134 @@ namespace Serenity.Data
         /// <returns></returns>
         public static SQLiteConnection Connect(DataScope scope)
         {
-            if (!Database.IsCreated(scope) && !Database.Create(scope))
+            if (!Database.IsCreated(scope))
             {
-                return null;
+                Database.Create(scope);
             }
-            //TODO: Implement support for multiple scopes.
-            if (scope == DataScope.Global)
-            {
-                var csb = new SQLiteConnectionStringBuilder();
-                csb.DataSource = Database.GlobalDBPath;
-                return new SQLiteConnection(csb.ConnectionString);
-            }
-            else if (scope == DataScope.Module)
-            {
-                if (Module.Current == null)
-                {
-                    return null;
-                }
-                throw new NotImplementedException();
-            }
-            else if (scope == DataScope.Domain)
-            {
-                if (Domain.Current == null || Module.Current == null)
-                {
-                    return null;
-                }
-                throw new NotImplementedException();
-            }
-            else
-            {
-                throw new ArgumentException(__Strings.Exceptions.UnrecognizedDataScope);
-            }
+
+            return new SQLiteConnection("Data Source=" + Database.GetDatafilePath(scope));
         }
+        /// <summary>
+        /// Determines if the database of the specified scope has been created.
+        /// </summary>
+        /// <param name="scope"></param>
+        /// <returns></returns>
         public static bool IsCreated(DataScope scope)
         {
-            //TODO: Implement support for multiple scopes.
+            return File.Exists(Database.GetDatafilePath(scope));
+        }
+        /// <summary>
+        /// Gets the path of the global datafile.
+        /// </summary>
+        /// <returns></returns>
+        public static string GetDatafilePath()
+        {
+            return Path.Combine(SerenityPath.DataDirectory, "global/database");
+        }
+        /// <summary>
+        /// Gets the path of the datafile of the specified module.
+        /// </summary>
+        /// <param name="module"></param>
+        /// <returns></returns>
+        public static string GetDatafilePath(string module)
+        {
+            return Path.Combine(SerenityPath.DataDirectory,
+                Path.Combine(Path.GetFileNameWithoutExtension(module), "database"));
+        }
+        /// <summary>
+        /// Gets the path of the datafile of the specified module and domain.
+        /// </summary>
+        /// <param name="module"></param>
+        /// <param name="domain"></param>
+        /// <returns></returns>
+        public static string GetDatafilePath(string module, string domain)
+        {
+            return Path.Combine(SerenityPath.DataDirectory,
+                Path.Combine(Path.GetFileNameWithoutExtension(module),
+                    Path.Combine(Path.GetFileNameWithoutExtension(domain), "database")));
+        }
+        /// <summary>
+        /// Gets the path of the datafile of the specified scope.
+        /// </summary>
+        /// <param name="scope"></param>
+        /// <returns></returns>
+        public static string GetDatafilePath(DataScope scope)
+        {
             if (scope == DataScope.Global)
             {
-                return File.Exists(Database.GlobalDBPath);
+                return Database.GetDatafilePath();
             }
             else if (scope == DataScope.Module)
             {
                 if (Module.Current == null)
                 {
-                    return false;
+                    return null;
                 }
-                throw new NotImplementedException();
+
+                return Database.GetDatafilePath(Module.Current.Name);
             }
             else if (scope == DataScope.Domain)
             {
                 if (Domain.Current == null || Module.Current == null)
                 {
-                    return false;
+                    return null;
                 }
-                throw new NotImplementedException();
+                return Database.GetDatafilePath(Module.Current.Name, Domain.Current.HostName);
             }
             else
             {
                 throw new ArgumentException(__Strings.Exceptions.UnrecognizedDataScope);
             }
         }
-        #endregion
-        #region Properties - Public
-        public static string GlobalDBPath
+        /// <summary>
+        /// Gets the path of the database schema file for the global scope.
+        /// </summary>
+        /// <returns></returns>
+        public static string GetSchemaPath()
         {
-            get
-            {
-                return Path.Combine(SerenityPath.DataDirectory, "global/database");
-            }
+            return Path.Combine(SerenityPath.DataDirectory, "global/schema.sql");
         }
-        public static string GlobalSchemaPath
+        /// <summary>
+        /// Gets the path of the database schema file for the specified module.
+        /// </summary>
+        /// <param name="module"></param>
+        /// <returns></returns>
+        public static string GetSchemaPath(string module)
         {
-            get
+            return Path.Combine(SerenityPath.DataDirectory,
+                Path.Combine(Path.GetFileNameWithoutExtension(module), "schema.sql"));
+        }
+        /// <summary>
+        /// Gets the path of the database schema file for the specified scope.
+        /// </summary>
+        /// <param name="scope"></param>
+        /// <returns></returns>
+        public static string GetSchemaPath(DataScope scope)
+        {
+            if (scope == DataScope.Global)
             {
-                return Path.Combine(SerenityPath.DataDirectory, "global/schema.sql");
+                return Database.GetSchemaPath();
+            }
+            else if (scope == DataScope.Module)
+            {
+                if (Module.Current == null)
+                {
+                    return null;
+                }
+
+                return Database.GetSchemaPath(Module.Current.Name);
+            }
+            else if (scope == DataScope.Domain)
+            {
+                if (Domain.Current == null || Module.Current == null)
+                {
+                    return null;
+                }
+                return Database.GetSchemaPath(Module.Current.Name);
+            }
+            else
+            {
+                throw new ArgumentException(__Strings.Exceptions.UnrecognizedDataScope);
             }
         }
         #endregion
