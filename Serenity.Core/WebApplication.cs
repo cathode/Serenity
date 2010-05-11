9cb48df41a8cf19d1a1626efa8ebf44cb986418f
cathode <cathode@7c140d53-a5f7-2844-acf0-7413b3fec49d>
2010-05-11 06:03:09 +0000

    * Added new Serenity.Core project to hold future rewrite of core API.
    git-svn-id: svn://gearedstudios.com/serenity@303 7c140d53-a5f7-2844-acf0-7413b3fec49d

---
+++ Serenity.Core/WebApplication.cs
@@ -0,0 +1,16 @@
+﻿/* Serenity - The next evolution of web server technology.
+ * Copyright © 2006-2010 Will Shelley. All Rights Reserved. */
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Serenity.Core
+{
+    /// <summary>
+    /// Represents a web application.
+    /// </summary>
+    public abstract class WebApplication
+    {
+
+    }
+}
--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -1,25 +1,15 @@
-﻿/******************************************************************************
- * Serenity - The next evolution of web server technology.                    *
- * Copyright © 2006-2008 Serenity Project - http://SerenityProject.net/       *
- *----------------------------------------------------------------------------*
- * This software is released under the terms and conditions of the Microsoft  *
- * Public License (Ms-PL), a copy of which should have been included with     *
- * this distribution as License.txt.                                          *
- *****************************************************************************/
+﻿/* Serenity - The next evolution of web server technology.
+ * Copyright © 2006-2010 Will Shelley. All Rights Reserved. */
 using System;
 using System.Reflection;
 using System.Security.Permissions;
 
-using Serenity;
-
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany(SerenityInfo.Company)]
-[assembly: AssemblyProduct(SerenityInfo.Name)]
-[assembly: AssemblyCopyright(SerenityInfo.Copyright)]
+[assembly: AssemblyCompany("Geared Studios")]
+[assembly: AssemblyProduct("Serenity")]
+[assembly: AssemblyCopyright("Copyright © 2006-2010 Will Shelley")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion(SerenityInfo.Version)]
+[assembly: AssemblyVersion("0.7.*")]
 
 [assembly: CLSCompliant(false)]
-
-// [assembly: SecurityPermission(SecurityAction.RequestMinimum, Execution=true)]
