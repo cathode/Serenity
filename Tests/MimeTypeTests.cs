850cd7a3c5647c130f3f94ee73edd898b978306b
cathode <cathode@7c140d53-a5f7-2844-acf0-7413b3fec49d>
2010-05-12 06:25:40 +0000

    * Added tests for MimeType class.
    git-svn-id: svn://gearedstudios.com/serenity@310 7c140d53-a5f7-2844-acf0-7413b3fec49d

---
+++ Tests/MimeTypeTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using Serenity.Core;
+
+namespace Tests
+{
+    [TestFixture]
+    public class MimeTypeTests
+    {
+        public void ParseShouldSucceedWithValidInput()
+        {
+            string[] inputs = {
+                                  "text/plain",
+                                  "application/xml",
+                                  "image/png",
+                                  "audio/vnd.rn-realaudio",
+                                  "application/xhtml+xml",
+                                  "multipart/form-data"
+                              };
+            MimeType[] outputs = {
+                                     MimeType.TextPlain,
+                                     MimeType.ApplicationXml,
+                                     MimeType.ImagePng,
+                                     MimeType.AudioVendorRNRealAudio,
+                                     MimeType.ApplicationXhtmlPlusXml,
+                                     MimeType.MultipartFormData
+                                 };
+
+            for (int i = 0; i < inputs.Length; i++)
+            {
+                try
+                {
+                    var parsed = MimeType.Parse(inputs[i]);
+
+                    Assert.AreEqual(outputs[i], parsed);
+                }
+                catch (Exception ex)
+                {
+                    Assert.Fail(string.Format("Failed to parse mimetype input: \"{0}\". Exception: {1}", inputs[i], ex.ToString()));
+                }
+            }
+        }
+    }
+}
