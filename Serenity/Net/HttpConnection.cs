0287d9e1be1c16098a1e57eb5be9fe148a8e5083
cathode <cathode@7c140d53-a5f7-2844-acf0-7413b3fec49d>
2011-10-17 03:36:05 +0000

    * Started implementing new side-by-side HTTP networking API.
    git-svn-id: svn://gearedstudios.com/serenity@330 7c140d53-a5f7-2844-acf0-7413b3fec49d

---
+++ Serenity/Net/HttpConnection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Serenity.Net
+{
+    /// <summary>
+    /// Implements a client-server link that exchanges requests and responses using the HTTP protocol.
+    /// </summary>
+    public class HttpConnection
+    {
+    }
+}
---
+++ Serenity/Net/HttpListenerAsync.cs
@@ -0,0 +1,59 @@
+﻿/******************************************************************************
+ * Serenity - Managed Web Application Server. ( http://gearedstudios.com/ )   *
+ * Copyright © 2006-2011 William 'cathode' Shelley. All Rights Reserved.      *
+ * This software is released under the terms and conditions of the MIT/X11    *
+ * license; see the included 'license.txt' file for the full text.            *
+ *****************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics.Contracts;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Serenity.Net
+{
+    /// <summary>
+    /// Provides a socket listen server that accepts incoming HTTP connections.
+    /// </summary>
+    public class HttpListenerAsync
+    {
+        #region Fields
+        public const int DefaultPort = 80;
+        private int port;
+        #endregion
+        #region Constructors
+        public HttpListenerAsync()
+        {
+            this.port = HttpListenerAsync.DefaultPort;
+        }
+        public HttpListenerAsync(int port)
+        {
+        }
+        #endregion
+        #region Properties
+        public int Port
+        {
+            get
+            {
+                return this.port;
+            }
+            set
+            {
+                Contract.Requires(value >= ushort.MinValue);
+                Contract.Requires(value <= ushort.MaxValue);
+
+                this.port = value;
+            }
+        }
+        #endregion
+        #region Methods
+        protected virtual void AcceptAsync(SocketAsyncEventArgs e)
+        {
+
+        }
+
+        #endregion
+    }
+}
